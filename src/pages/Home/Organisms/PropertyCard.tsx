959031ad1c1d0a51ba527305625893e703ee4ab3
dwikiop <dwikiokvianp@gmail.com>
2023-03-19 23:10:47 +0700

    feat - add icon in card

--- src/pages/Home/Organisms/PropertyCard.tsx
+++ src/pages/Home/Organisms/PropertyCard.tsx
@@ -1,15 +1,26 @@
+import { AiFillHeart, AiFillStar, AiOutlineHeart } from "react-icons/ai";
+
 const PropertyCard = () => {
   return (
     <>
       <section>
         <div className="p-6 text-md">
-          <img
-            src="https://a0.muscache.com/im/pictures/1bcb6274-d94f-4932-8c6e-dd61ebb98e38.jpg?im_w=320"
-            alt=""
-            className="w-full aspect-[4/4] rounded-xl"
-          />
+          <div className="w-full">
+            <img
+              src="https://a0.muscache.com/im/pictures/426e8d8f-aa0a-485e-9106-92699101f9c2.jpg?im_w=320"
+              alt=""
+              className="w-full aspect-[4/4] rounded-xl"
+            />
+            <AiOutlineHeart className="" />
+          </div>
           <div className="mt-2">
-            <p className="font-bold">Seririt, Indonesia</p>
+            <div className="flex justify-between ">
+              <p className="font-bold">Seririt, Indonesia</p>
+              <div className="flex">
+                <AiFillStar />
+                <p>4.89</p>
+              </div>
+            </div>
             <p>Berjarak 916 kilometer</p>
             <p>22-27 Mei</p>
             <p>
