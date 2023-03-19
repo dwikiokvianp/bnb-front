171185709001588e938c8fbb7303a9d252063292
dwikiop <dwikiokvianp@gmail.com>
2023-03-20 01:32:59 +0700

    feat - fix position

--- src/pages/Home/Organisms/UserNavigation.tsx
+++ src/pages/Home/Organisms/UserNavigation.tsx
@@ -21,15 +21,15 @@ const UserNavigation = () => {
             <p className="ml-2 inline underline">Pelajari lebih lanjut</p>
           </div>
         </section>
-        <section>
-          <div className="flex p-4 px-5 border 2xl:w-[80%] 2xl:mx-auto items-center">
+        <section className="border-b-2 border-t-2">
+          <div className="flex p-4 px-5 2xl:w-[80%] 2xl:mx-auto items-center">
             <CompanyLogo />
             <SearchBarDesktop />
             <div className="flex flex-1 justify-end gap-x-4 ">
-              <div className="flex items-center p-2 px-3 hover:bg-slate-50 rounded-2xl text-slate-600 font-semibold">
+              <div className="flex items-center p-2 px-3 hover:bg-slate-100 rounded-2xl text-slate-600 font-semibold">
                 Jadikan rumah Anda Airbnb
               </div>
-              <div className="flex items-center text-xl">
+              <div className="flex items-center text-xl hover:bg-slate-100 rounded-full p-3">
                 <BsGlobe />
               </div>
               <div className="flex border-2 hover:shadow-lg rounded text-2xl justify-center items-center px-4 py-2 rounded-3xl gap-1">
